7714ba46f36af1308cf5b52ddc9309b21265b5c5
Torey <blueberryuec@gmail.com>
2020-10-27 19:25:05 +0800

    Update delete_multi_iam_user.py

--- delete_multi_iam_user.py
+++ delete_multi_iam_user.py
@@ -4,11 +4,13 @@ iam = boto3.client('iam')
 a = 'user0'
 for i in range(1,10):
   name = a + str(i)
+  iam.delete_login_profile(UserName=name)
   iam.delete_user(UserName=name)
 
 a = 'user'
 for i in range(10,51):
   name = a + str(i)
+  iam.delete_login_profile(UserName=name)
   iam.delete_user(UserName=name)
 
 iam.delete_group(
